557d13154c0f6bfe2ff2f65ec20cd96ef81a7b04
Rebecca Cairns <rebecca_cairns@yahoo.com>
2019-02-22 18:27:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -70,6 +70,6 @@ function placeOrder(cardNumber = 0) {
     return 'Sorry, we don\'t have a credit card on file for you.';
   }
   if (cardNumber > 0) {
-    return "Your total cost is $" + total() + ", which will be charged to the card." + cardNumber;
+    return "Your total cost is $" + total() + ", which will be charged to the card " + cardNumber + ".";
   }
 }
